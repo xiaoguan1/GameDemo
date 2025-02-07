5b8f3b60ca36c9ea222a33ef90d25f9f11a42cba
guanguopwei <13249153637@qq.com>
2025-02-07 17:46:29 +0800

    添加多层级创建文件夹

--- tool/luaplugins/preload.lua
+++ tool/luaplugins/preload.lua
@@ -40,3 +40,27 @@ skynet.register_protocol({
 	unpack = skynet.unpack,
 	pack = skynet.pack,
 })
+
+local posix = require "posix"
+function posix.mkdir_p(path)
+	if not path then
+		return
+	end
+	local loop, index = 1, 1
+	while true do
+		if loop >= 100 then
+			error("loop too must")
+		end
+		loop = loop + 1
+		local sIndex = string.find(path, "/", index)
+		if not sIndex then
+			break
+		end
+		index = sIndex + 1
+		local p = string.sub(path, 1, sIndex)
+		if not p then
+			error("path sub error!")
+		end
+		posix.mkdir(p)
+	end
+end
