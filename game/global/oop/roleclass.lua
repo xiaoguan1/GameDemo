a93941e19ef07ee5893f0f67cc8d48be802c24f3
guanguopwei <13249153637@qq.com>
2025-05-15 01:55:48 +0800

    玩家类

---
+++ game/global/oop/roleclass.lua
@@ -0,0 +1,80 @@
+-----------------------
+--- 玩家类模板
+-----------------------
+
+-- 在线玩家对象 ------
+ROLE_OBJS = {}
+
+function GetAllRoleObjs()
+	return ROLE_OBJS
+end
+
+function GetRoleObjByUid(uid)
+	return uid and ROLE_OBJS[uid]
+end
+
+-- 玩家类 ------
+RoleClass = { __ClassType = "<<role class>>" }
+function RoleClass:New(o)
+	o = o or {}
+	if type(o.uid) ~= "string" or ROLE_OBJS[o.uid] then
+		_ERROR_F("RoleClass new fail! o:%s", tool.dumptree(o))
+		return
+	end
+
+	o.__SuperClass = self	-- 标记RoleClass为父类
+	o.__ObjectType = true	-- 标记为实例对象
+	o.__TempData = {}		-- 重置临时数据
+
+	-- 直接将RoleClass放入o原表的__index里面。
+	-- 	若o已设置A函数 且 RoleClass也设置A函数，o:A() 会调用o的而非RoleClass
+	setmetatable(o, {__index = self})
+
+	ROLE_OBJS[o.uid] = o
+end
+
+function RoleClass:GetUid()
+	return self.uid
+end
+
+-- 存盘数据
+function RoleClass:SetData(fieldName, data)
+	self.__Data[fieldName] = data
+end
+function RoleClass:GetData(fieldName)
+	return self.__Data[fieldName]
+end
+
+-- 临时数据（上线、下线就没有了）
+function RoleClass:SetTempData(fieldName, data)
+	self.__TempData[fieldName] = data
+end
+function RoleClass:GetTempData(fieldName)
+	return self.__TempData[fieldName]
+end
+
+--autogen-begin
+function RoleClass:GetSex()
+    return self.__Data.Sex
+end
+function RoleClass:SetSex(Sex)
+    self.__Data.Sex = Sex
+end
+
+function RoleClass:GetLike1()
+    return self.__TempData.Like1
+end
+function RoleClass:SetLike1(Like1)
+    self.__TempData.Like1 = Like1
+end
+
+function RoleClass:GetLike2()
+    return self.__TempData.Like2
+end
+function RoleClass:SetLike2(Like2)
+    self.__TempData.Like2 = Like2
+end
+
+
+--autogen-end
+
---
+++ game/global/oop/roleval.lua
@@ -0,0 +1,92 @@
+
+DbField = {
+	"Sex",
+}
+
+TempField = {
+	"Like1",
+	"Like2",
+}
+
+
+-- 字段检查
+function FieldCheck(ft1, ft2)
+	assert(ft1 and ft2)
+
+	local function _ReverseMap(ft, tf)
+		for k, fieldName in pairs(ft) do
+			if tf[fieldName] then
+				error("repeat field " .. fieldName)
+			end
+			tf[fieldName] = k
+		end
+	end
+	local function _DoCheck(tf1, tf2)
+		for fieldName in pairs(tf1) do
+			-- 存盘需要简单的命名方式，方便阅读与修改
+			if not string.match(fieldName, "^[%a_][%w_]*$") then
+				error(string.format("%s is not valid var name", fieldName))
+			end
+
+			if tf2[fieldName] then
+				error(string.format("TempField has DbField.%s error!", fieldName))
+			end
+		end
+	end
+
+	local tf1, tf2 = {}, {}
+	_ReverseMap(ft1, tf1)
+	_ReverseMap(ft2, tf2)
+	_DoCheck(tf1, tf2)
+	_DoCheck(tf2, tf1)
+end
+FieldCheck(DbField, TempField)
+
+
+local VAR_PATTERN_FORMAT = [[
+function RoleClass:Get%s()
+    return self.%s.%s
+end
+function RoleClass:Set%s(%s)
+    self.%s.%s = %s
+end
+
+]]
+
+local strList = ""
+function DoAutoCreate(fieldList, saveStr)
+	for _, field in pairs(fieldList) do
+		local str = string.format(VAR_PATTERN_FORMAT,
+						field, saveStr, field,
+						field, field, saveStr, field, field
+					)
+		strList = strList .. str
+	end
+end
+DoAutoCreate(DbField, "__Data")
+DoAutoCreate(TempField, "__TempData")
+strList = "\n" .. strList .. "\n"
+
+function GenFile(data)
+	local filePath = "./game/global/oop/roleclass.lua"
+	local content
+	local rf = io.open(filePath, "r")
+	if rf then 
+		content = rf:read("*a")
+		rf:close()
+    end
+
+	if content then
+		local sub
+		data, sub = string.gsub(content, "(%-%-autogen%-begin).-(%-%-autogen%-end)", "%1" .. data .. "%2")
+        assert(sub == 1, string.format("must insert into file: %s once", filePath))
+    else
+        error("not file: " .. filePath)
+    end
+
+	local fd = assert(io.open(filePath, "w"))
+	fd:write(data)
+	fd:flush()
+	fd:close()
+end
+GenFile(strList)
