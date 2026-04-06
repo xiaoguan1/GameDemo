3c21c9d666d9c19c1939cca95869831487e67d0f
xiaoguan1 <1326317931@qq.com>
2026-04-07 00:26:10 +0800

    添加注释

--- game/service/manage/handle/ddbug.lua
+++ game/service/manage/handle/ddbug.lua
@@ -1,7 +1,12 @@
 local skynet = require "skynet"
 
--- wget -q -O - "http://127.0.0.1:40001/ddbug" 或者 curl "http://127.0.0.1:40001/ddbug"
--- 携带参数：wget -q -O - "http://127.0.0.1:40001/ddbug?key1=value1&key2=value2。。。"
+--[[
+wget -q -O - "http://127.0.0.1:40001/ddbug"
+或者 curl "http://127.0.0.1:40001/ddbug"
+
+携带参数：wget -q -O - "http://127.0.0.1:40001/ddbug?key1=value1&key2=value2。。。"
+或者 curl "http://127.0.0.1:40001/ddbug?key1=value1&key2=value2。。。"
+]]
 
 -- local ROLECLASS = Import("game/global/oop/roleclass.lua")
 -- Obj = {uid = "1111111111111"}
