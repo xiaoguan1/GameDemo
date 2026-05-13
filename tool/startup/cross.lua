b0467ac6bac4e2e44b8f280482e9c4723399b849
xiaoguan1 <1326317931@qq.com>
2026-05-13 17:20:48 +0800

    因为调整集群rpc故暂时先屏蔽跨服的启动细节

--- tool/startup/cross.lua
+++ tool/startup/cross.lua
@@ -44,17 +44,17 @@ skynet.start(function ()
 	DPCLUSTER_NODE = dpcluster
 
 	dofile "./game/global/log.lua"
-	for _, v in pairs(START_UNIQ_SERVICE) do
-		local id = skynet.uniqueservice(v.svr)
-		skynet.name(v.named, id)
-	end
-	Import("game/global/dpcluster.lua")
+	-- for _, v in pairs(START_UNIQ_SERVICE) do
+	-- 	local id = skynet.uniqueservice(v.svr)
+	-- 	skynet.name(v.named, id)
+	-- end
+	-- Import("game/global/dpcluster.lua")
 
-	local self_ipport = DPCLUSTER_NODE.node_ipport
-	for _, service in pairs(CROSS_SERVICE_STARTSEQ) do
-		local id = skynet.newservice(service)
-		skynet.name(CROSS_SERVICE_CFG[service].named, id)
-	end
+	-- local self_ipport = DPCLUSTER_NODE.node_ipport
+	-- for _, service in pairs(CROSS_SERVICE_STARTSEQ) do
+	-- 	local id = skynet.newservice(service)
+	-- 	skynet.name(CROSS_SERVICE_CFG[service].named, id)
+	-- end
 
 
 
